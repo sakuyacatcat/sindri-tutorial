5847d0564127cc6cf3738cfa59d9b5736d9f615c
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-01-31 01:40:18 +0900

    replace

--- backend/src/sdk/sindri-sdk.ts
+++ backend/src/sdk/sindri-sdk.ts
@@ -4,12 +4,15 @@ import sindri from "sindri";
 import { fileURLToPath } from "url";
 
 const dirName = dirname(fileURLToPath(import.meta.url));
-dotenv.config({ path: join(dirName, "../.env") });
+dotenv.config({ path: join(dirName, "../../.env") });
 
 async function main() {
   sindri.authorize({ apiKey: process.env.SINDRI_API_KEY });
-  const circuit = await sindri.getCircuit(process.env.SINDRI_CIRCUIT_ID || "");
-  console.log(circuit);
+  const proof = await sindri.proveCircuit(
+    process.env.SINDRI_CIRCUIT_ID || "",
+    '{"X":1,"Y":1}'
+  );
+  console.log(proof);
 }
 
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
