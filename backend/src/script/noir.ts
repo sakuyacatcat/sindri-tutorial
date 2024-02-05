610f27111a60e82c00295ed5d55c9e923ddb2b82
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-02-06 01:41:04 +0900

    experimental code

--- backend/src/script/noir.ts
+++ backend/src/script/noir.ts
@@ -8,33 +8,52 @@ type ProofInput = {
   input: number;
 };
 
-function initNoirClient() {
-  const circuitJson = circuit as CompiledCircuit;
-  const backend = new BarretenbergBackend(circuitJson);
-  const noir = new Noir(circuitJson, backend);
-  return noir;
-}
+class NoirClient {
+  private noir: Noir;
 
-async function getProof(noir: Noir, input: ProofInput): Promise<ProofData> {
-  console.log("logs", "Generating proof... ⌛");
-  const proof = await noir.generateFinalProof(input);
-  console.log("logs", "Generating proof... ✅");
-  console.log("results", proof.proof);
-  return proof;
-}
+  constructor(circuitJson: CompiledCircuit) {
+    const backend = new BarretenbergBackend(circuitJson);
+    this.noir = new Noir(circuitJson, backend);
+  }
 
-async function verifyProof(noir: Noir, proof: ProofData): Promise<boolean> {
-  console.log("logs", "Verifying proof... ⌛");
-  const verification = await noir.verifyFinalProof(proof);
-  return verification;
+  async generateProof(input: ProofInput): Promise<ProofData> {
+    console.log("logs", "Generating proof... ⌛");
+    try {
+      const proof = await this.noir.generateFinalProof(input);
+      console.log("logs", "Generating proof... ✅");
+      console.log("results", proof.proof);
+      return proof;
+    } catch (error) {
+      console.error("Error generating proof", error);
+      throw error;
+    }
+  }
+
+  async verifyProof(proof: ProofData): Promise<boolean> {
+    console.log("logs", "Verifying proof... ⌛");
+    try {
+      const verification = await this.noir.verifyFinalProof(proof);
+      if (verification) console.log("logs", "Verifying proof... ✅");
+      return verification;
+    } catch (error) {
+      console.error("Error verifying proof", error);
+      throw error;
+    }
+  }
 }
 
 async function main() {
-  const client = initNoirClient();
-  const input = { input: 20 };
-  const proof = await getProof(client, input);
-  const verification = await verifyProof(client, proof);
-  if (verification) console.log("logs", "Verifying proof... ✅");
+  try {
+    const client = new NoirClient(circuit as CompiledCircuit);
+    const input = { input: 20 };
+    const proof = await client.generateProof(input);
+    const verification = await client.verifyProof(proof);
+    if (verification) {
+      console.log("Proof verified successfully");
+    }
+  } catch (error) {
+    console.error("An error occurred in main function", error);
+  }
 }
 
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
